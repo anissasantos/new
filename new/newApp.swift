dff9c7b09f05bef5b8d806bd32abf506a80d144f
Anissa Santos <anissa.n.santos@gmail.com>
2023-07-13 13:30:28 -0500

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
